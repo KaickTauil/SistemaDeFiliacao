3ac690432da25667bd6eaab05dfde2b57103779c
teuac <cauetcampos@outlook.com>
2024-06-03 23:05:06 -0300

    projeto criado

---
+++ src/main/java/br/com/fetese/projetofetese/ProjetoFeteseApplication.java
@@ -0,0 +1,13 @@
+package br.com.fetese.projetofetese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFeteseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoFeteseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fetese/projetofetese/ProjetoFeteseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fetese.projetofetese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFeteseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
